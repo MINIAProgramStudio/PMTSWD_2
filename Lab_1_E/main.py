78b46b980abcae4e6a08b3bd1fb216e01529654c
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-19 16:56:41 +0300

    stdin changed to input, stdout changed to print

--- Lab_1_E/main.py
+++ Lab_1_E/main.py
@@ -5,13 +5,29 @@ import sys
 DEFAULT_TOP_PATH = "\\"
 
 def get_args(recived_input_line):
-    file_name = recived_input_line[recived_input_line.find('-f ')+3:recived_input_line.find(';',)]
-    top_path = recived_input_line[recived_input_line.find(' ')+1:]
+    global DEFAULT_TOP_PATH
+    minus_f = recived_input_line.find('-f ')
+    minus_p = recived_input_line.find('-p ')
+    minus_d = recived_input_line.find('-d ')
 
-    if not os.path.isdir(top_path):
-        sys.stderr.write("ERR_GA_1: No such directory")
-        return -1
-    return [file_name, top_path]
+    if not minus_f == -1:
+        file_name = recived_input_line[minus_f+3:recived_input_line.find(';',minus_f)]
+    else:
+        file_name = recived_input_line
+        top_path = DEFAULT_TOP_PATH
+
+    if not minus_p == -1:
+        top_path = recived_input_line[minus_p+3:recived_input_line.find(';',minus_p)]
+    else:
+        top_path = DEFAULT_TOP_PATH
+
+    if not minus_d == -1:
+        new_default_top_path = recived_input_line[minus_d+3:recived_input_line.find(';',minus_d)]
+        if not os.path.isdir(new_default_top_path):
+            sys.stderr.write("ERR_GA_1: New DEFAULT_TOP_PATH is not a valid path")
+        else:
+            DEFAULT_TOP_PATH = new_default_top_path
+    return [file_name,top_path]
 
 def find_file(name, top_path=DEFAULT_TOP_PATH):
     return_list = []
@@ -30,16 +46,19 @@ def find_file_from_stdin():
 
 # main loop
 while True:
-    for line in sys.stdin:
-        if 'Exit' == line.rstrip():
-            break
-        args = get_args(line.rstrip())
-        if isinstance(args,list):
-            file_list = find_file(args[0],args[1])
+    line = input()
+    if 'Exit' == line.rstrip():
+        break
+    args = get_args(line.rstrip())
+    if isinstance(args,list):
+        file_list = find_file(args[0],args[1])
+        if isinstance(file_list,list):
             for file in file_list:
-                sys.stdout.write(file)
+                print(file)
         else:
-            sys.stderr.write("ERR_MAIN_1: Arg check failed")
-            continue
+            sys.stderr.write("ERR_Main_2: No files found")
+    else:
+        sys.stderr.write("ERR_Main_1: Arguments check failed")
+        continue
 
 sys.exit()
