fc13ad0630c9f8d700f855635e812ed57eb31e67
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-19 15:50:05 +0300

    find_file finished

--- Lab_1_E/main.py
+++ Lab_1_E/main.py
@@ -1,12 +1,15 @@
 import os
 import sys
 
-def find_file(name, top_path="C:\\"):
+def find_file(name, top_path="\\"):
+    return_list = []
     for root, dirs, files in os.walk(top_path):
         if name in files:
-            return os.path.join(root, name)
-        else:
-            return -1
+            return_list.append(os.path.join(root, name))
+    if len(return_list)>0:
+        return return_list
+    else:
+        return -1 #no such file(-s) in the directory
 
 print(find_file("1.txt"))
 
