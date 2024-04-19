b70f48f724cb1cfdf9881ccdae06f1b673585655
Mykhailo Holub <mishataube2005@gmail.com>
2024-04-19 15:54:01 +0300

    get_lines_from_stdin implemented

--- Lab_1_E/main.py
+++ Lab_1_E/main.py
@@ -1,15 +1,27 @@
 import os
 import sys
 
+
+def get_lines_from_stdin():
+    recived_lines = []
+    for line in sys.stdin:
+        if line.rstrip in ['exit', 'Exit']:
+            sys.exit()
+        else:
+            recived_lines.append(line)
+    return recived_lines
+
+
 def find_file(name, top_path="\\"):
     return_list = []
     for root, dirs, files in os.walk(top_path):
         if name in files:
             return_list.append(os.path.join(root, name))
-    if len(return_list)>0:
+    if len(return_list) > 0:
         return return_list
     else:
-        return -1 #no such file(-s) in the directory
+        return -1  # no such file(-s) in the directory
+
 
 print(find_file("1.txt"))
 
