177c1f97cafdc1a81c57cb8e0e3f7a43176749e7
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-03 20:47:06 +0300

    Create LocomotiveBuilder.py

---
+++ Lab_2_E/LocomotiveBuilder.py
@@ -0,0 +1,15 @@
+class EmptyLocomotiveBuilder:
+    def __init__(self):
+        pass
+
+    def set_engine(self):
+        pass
+
+    def set_transmission(self):
+        pass
+
+    def set_wheels(self):
+        pass
+
+    def set_cab(self):
+        pass
