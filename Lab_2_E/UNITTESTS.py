f689f6225d727f0ad250698f6972161312516f32
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-10 12:45:10 +0300

    Engine unittests

--- Lab_2_E/UNITTESTS.py
+++ Lab_2_E/UNITTESTS.py
@@ -11,7 +11,44 @@ class TestLocoPartsEngine(unittest.TestCase):
         expected_output = "10000W engine that consumes 100L/h and weighs 100Kg"
         self.assertEqual(str(engine), expected_output)
 
-    def test_power_invalid_input_type(self):
+    @patch('sys.stderr.write')
+    def test_power_invalid_input_type(self,mock_err):
         with self.assertRaises(SystemExit) as cm:
             LocoParts.Engine(10000.0, 100, 100)
         self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: power must be int")
+
+    @patch('sys.stderr.write')
+    def test_power_invalid_input_value(self, mock_err):
+        with self.assertRaises(SystemExit) as cm:
+            LocoParts.Engine(0, 100, 100)
+        self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: power must be positive")
+
+    @patch('sys.stderr.write')
+    def test_fuel_consumption_invalid_type(self, mock_err):
+        with self.assertRaises(SystemExit) as cm:
+            LocoParts.Engine(10000, 100.0, 100)
+        self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: fuel_consumption must be int")
+
+    @patch('sys.stderr.write')
+    def test_fuel_consumption_invalid_value(self, mock_err):
+        with self.assertRaises(SystemExit) as cm:
+            LocoParts.Engine(10000, 0, 100)
+        self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: fuel_consumption must be positive")
+
+    @patch('sys.stderr.write')
+    def test_mass_invalid_type(self, mock_err):
+        with self.assertRaises(SystemExit) as cm:
+            LocoParts.Engine(10000, 100, 100.0)
+        self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: mass must be int")
+
+    @patch('sys.stderr.write')
+    def test_mass_invalid_value(self, mock_err):
+        with self.assertRaises(SystemExit) as cm:
+            LocoParts.Engine(10000, 100, 0)
+        self.assertEqual(cm.exception.code, -1)
+        mock_err.assert_called_with("ERR: mass must be positive")
