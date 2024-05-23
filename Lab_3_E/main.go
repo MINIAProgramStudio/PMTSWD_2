82b0cfdcbc6edc8b64eff9b844a9ca5919d2bf34
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-23 13:30:05 +0300

    eh... Two peperonies please

--- Lab_3_E/main.go
+++ Lab_3_E/main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
+func main() {
 	//value types
 	var int_value int
 	var float_value float32
@@ -11,21 +13,22 @@ func main(){
 	var byte_value byte
 	var rune_value rune
 
-	
 	int_value = 0 // zero jokes about integers. Integers are some very serious numbers.
 	// I am a...
 	float_value = 3.1415 // oneer in Go
 	string_value = "What a splended day, me Lord, aint it?"
 	bool_value = false // alarm, it was my clock
-	byte_value = 255 // bits gathered together and created a 2^255 number
-	rune_value = 'f'//bomb
+	byte_value = 255   // bits gathered together and created a 2^255 number
+	rune_value = 'f'   //bomb
 
 	fmt.Println("Integer example: ", int_value)
 	fmt.Println("Float example: ", float_value)
-	fmt.Println(string_value)
-	fmt.Println(bool_value)
-	fmt.Println(byte_value)
-	fmt.Println(rune_value)
+	fmt.Println("String example: ", string_value)
+	fmt.Println("Bool example: ", bool_value)
+	fmt.Println("Byte example: ", byte_value)
+	fmt.Println("Rune example: ", rune_value)
 
-	input := bufio.NewScanner(os.Stdin)
+	//wait for input
+	fmt.Print("Press the Enter Key to terminate the program")
+	fmt.Scanln()
 }
