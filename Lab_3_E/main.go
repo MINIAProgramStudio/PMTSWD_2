4bd337198f94e4e021649a3fe6199045c68b0b81
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-23 13:53:34 +0300

    v/r-type demonstration done

--- Lab_3_E/main.go
+++ Lab_3_E/main.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+type ValueType struct {
+	value int
+}
+
+// Change of value type
+func modify_value(v ValueType) {
+	v.value = 100
+	fmt.Println("Inside modify_value function:", v.value) // Має вивести: Inside modifyValue function: 100
+}
+
+type ReferenceType struct {
+	value int
+}
+
+// change
+func modify_reference(r *ReferenceType) {
+	r.value = 100
+	fmt.Println("Inside modify_reference function:", r.value) // Має вивести: Inside modifyReference function: 100
+}
+
 func main() {
 	//value types
 	var int_value int
@@ -27,6 +47,17 @@ func main() {
 	fmt.Println("Bool example: ", bool_value)
 	fmt.Println("Byte example: ", byte_value)
 	fmt.Println("Rune example: ", rune_value)
+	fmt.Println()
+
+	// Для значення типу
+	v := ValueType{value: 10}
+	modify_value(v)
+	fmt.Println("Outside modify_value function:", v.value) // Output: Outside modifyValue function: 10
+
+	// Для посилання типу
+	r := &ReferenceType{value: 10}
+	modify_reference(r)
+	fmt.Println("Outside modify_reference function:", r.value) // Output: Outside modifyReference function: 100
 
 	//wait for input
 	fmt.Print("Press the Enter Key to terminate the program")
