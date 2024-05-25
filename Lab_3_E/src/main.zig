0a75f6d3faa4988a95dc694527200cd10c4ecdd6
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-25 19:27:40 +0300

    Update main.zig

--- Lab_3_E/src/main.zig
+++ Lab_3_E/src/main.zig
@@ -20,14 +20,11 @@ pub fn main() !void {
     const stdout = bw.writer();
 
     // Демонстрація value type
-    var value_le_originale: i32 = 123;
-    var value_type: i32 = value_le_originale;
-    value_type = value_type - 10;
-    try stdout.print("after value type: {}\n", .{value_le_originale}); // хотів щоб виводило українською, але Zig іншої думки про це
-    var reference_type: *i32 = &value_le_originale;
-    reference_type.* = reference_type.* + 10;
-    try stdout.print("after reference type: {}\n", .{value_le_originale});
-    reference_type = &value_type; // щоб не було константою
-    value_le_originale = 0; // щоб не було константою
+    const original_instance = ExampleObject{ .value = 0 };
+    change_by_value_type(original_instance);
+
+    try stdout.print("{}\n", .{original_instance.value}); // хотів щоб виводило українською, але Zig іншої думки про це
+
+    original_instance.deinit()
     try bw.flush(); // за собою необхідно змивати! (:
 }
