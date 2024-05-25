fe7162b61fca1bf13b6cc9008c2dc00496109f3f
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-25 19:06:02 +0300

    Examplebject, cbvt, cbrt implemented

--- Lab_3_E/src/main.zig
+++ Lab_3_E/src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
+const ExampleObject = struct {
+    value: i32,
+};
+
+fn change_by_value_type(InputObject: ExampleObject) ExampleObject {
+    InputObject.value = InputObject.value + 10;
+    return InputObject;
+}
+
+fn change_by_reference_type(InputObject: *ExampleObject) *ExampleObject {
+    InputObject.value = InputObject.value + 10;
+    return InputObject;
+}
+
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
