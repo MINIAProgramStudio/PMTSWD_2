2da78f0376e1958ce917420b335441b58efad188
Mykhailo Holub <mishataube2005@gmail.com>
2024-05-27 14:30:59 +0300

    it works... somehow

--- Lab_3_E/src/main.zig
+++ Lab_3_E/src/main.zig
@@ -4,14 +4,14 @@ const ExampleObject = struct {
     value: i32,
 };
 
-fn change_by_value_type(InputObject: ExampleObject) ExampleObject {
-    InputObject.value = InputObject.value + 10;
-    return InputObject;
+fn change_by_value_type(input_object: ExampleObject) ExampleObject {
+    var temp_object = input_object;
+    temp_object.value = temp_object.value + 10;
+    return temp_object;
 }
 
-fn change_by_reference_type(InputObject: *ExampleObject) *ExampleObject {
-    InputObject.value = InputObject.value + 10;
-    return InputObject;
+fn change_by_reference_type(input_object: *ExampleObject) *ExampleObject {
+    input_object.value += 10;
 }
 
 pub fn main() !void {
@@ -20,11 +20,14 @@ pub fn main() !void {
     const stdout = bw.writer();
 
     // Демонстрація value type
-    const original_instance = ExampleObject{ .value = 0 };
-    change_by_value_type(original_instance);
+    var original_instance = ExampleObject{ .value = 0 };
+    var changed_value_type = change_by_value_type(original_instance);
 
     try stdout.print("{}\n", .{original_instance.value}); // хотів щоб виводило українською, але Zig іншої думки про це
-
-    original_instance.deinit()
+    try stdout.print("{}\n", .{changed_value_type.value});
     try bw.flush(); // за собою необхідно змивати! (:
+
+    // я не знаю чому, але це потрібно щоб воно компілювалось :/
+    original_instance = ExampleObject{ .value = 0 };
+    changed_value_type = ExampleObject{ .value = 0 };
 }
